eff34053cac203040077ee018cd83fa4a83dd29d
nikitaizosimov <48530918+nikitaizosimov@users.noreply.github.com>
2020-08-31 18:45:49 +0300

    Add files via upload

---
+++ 1-0-ulstr.c
@@ -0,0 +1,27 @@
+
+#include <unistd.h>
+
+int     main(int argc,char **argv)
+{
+    if (argc == 2)
+    {
+        while (*argv[1])
+        {
+            if (*argv[1] >= 'a' && *argv[1] <= 'z')
+            {
+                *argv[1] -= 32;
+                write(1, argv[1], 1);
+            }
+            else if (*argv[1] >= 'A' && *argv[1] <= 'Z')
+            {
+                *argv[1] += 32;
+                write(1, argv[1], 1);
+            }
+            else
+                write(1, argv[1], 1);
+            argv[1]++;
+        }
+    }
+    write(1, "\n", 1);
+    return (0);
+}
