87526b63c56efabee9ec8d87de15bb2b94bb2f68
nikitaizosimov <48530918+nikitaizosimov@users.noreply.github.com>
2020-09-02 21:27:23 +0300

    Add files via upload

---
+++ 2-1-alpha_mirror.c
@@ -0,0 +1,27 @@
+
+#include <unistd.h>
+
+int    main(int argc,char **argv)
+{
+    int temp;
+    if (argc == 2)
+    {
+        while (*argv[1])
+        {
+            if (*argv[1] >= 'a' && *argv[1] <='z') {
+                temp = 'z' - (*argv[1] - 'a');
+                write(1, &temp, 1);
+            } else if (*argv[1] >= 'A' && *argv[1] <='Z')
+            {
+                temp = 'Z' - (*argv[1] - 'A');
+                write(1, &temp, 1);
+            } else
+            {
+                write(1, argv[1], 1);
+            }
+            argv[1]++;
+        }
+    }
+    write(1, "\n", 1);
+    return (0);
+}
