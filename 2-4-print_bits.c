afee65bc8da6302dee508d6004beb3e8915ad6c0
nikitaizosimov <48530918+nikitaizosimov@users.noreply.github.com>
2020-09-03 19:34:54 +0300

    Add files via upload

---
+++ 2-4-print_bits.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+void	print_bits(unsigned char octet)
+{
+	int	i = 256;
+	while (i >>= 1)
+		(octet & i) ? write(1, "1", 1) : write(1, "0", 1);
+}
+
+int		main(void)//
+{//
+	int n = 64;
+	print_bits(n);//
+	write(1, "\n", 1);//
+	print_bits2(n);//
+}//
